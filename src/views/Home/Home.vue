bde002ccb10fcf51d2a71f287556e966b3fb4841
ArdianRahmanandaSaputra <ardianrahmanandasaputra12345@gmail.com>
2022-11-16 07:05:58 +0700

    commit

--- src/views/Home/Home.vue
+++ src/views/Home/Home.vue
@@ -1,42 +1,54 @@
 <template>
-	<Sidebar />
-	<div class="container-fluid">
-		<div class="mt-3">
-			<h2>Dashboard</h2><br>
-			<div class="row">
-					<div class="col-xl-4 col-sm-6 mb-3">
-						<div class="card text-white bg-primary o-hidden h-100">
-							<div class="card-body">
-								<div class="mr-5"><h3><span class="material-icons">group</span> Anggota</h3></div>				
-								<div class="mr-5"><h2>10</h2></div>
-							</div>
-						</div>
-					</div>
-					<div class="col-xl-4 col-sm-6 mb-3">
-						<div class="card text-white bg-warning o-hidden h-100">
-							<div class="card-body">
-								<div class="card-body-icon">
-									<i class="fas fa-notes-medical"></i>
-								</div>
-								<div class="mr-5">Pasien Lama</div>
-							</div>
-						</div>
-					</div>
-					<div class="col-xl-4 col-sm-6 mb-3">
-						<div class="card text-white bg-success o-hidden h-100">
-							<div class="card-body">
-								<div class="card-body-icon">
-									<i class="fad fa-print"></i>
-								</div>
-								<div class="mr-5">Cetak</div>
-							</div>
-			
-						</div>
-					</div>
-				</div>
-		</div>
-    
-	</div>
+  <Sidebar />
+  <div class="container-fluid">
+    <div class="mt-3">
+      <h2>Dashboard</h2>
+      <div class="mt-3">
+        <div class="row">
+          <div class="col-xl-4 col-sm-6 mb-3">
+            <div class="card text-white bg-primary o-hidden h-100">
+              <div class="card-body">
+                <div class="mr-5">
+                  <i class="far fa-users"></i> Anggota: 100
+                </div>
+              </div>
+              <RouterLink class="nav-link" to="/anggota"
+                ><a href="" class="btn btn-primary">
+                  Detail <i class="fa fa-arrow-right"></i
+                ></a>
+              </RouterLink>
+            </div>
+          </div>
+          <div class="col-xl-4 col-sm-6 mb-3">
+            <div class="card text-white bg-success o-hidden h-100">
+              <div class="card-body">
+                <div class="mr-5"><i class="far fa-books"></i> Buku: 100</div>
+              </div>
+              <RouterLink class="nav-link" to="/buku"
+                ><a href="" class="btn btn-success">
+                  Detail <i class="fa fa-arrow-right"></i
+                ></a>
+              </RouterLink>
+            </div>
+          </div>
+          <div class="col-xl-4 col-sm-6 mb-3">
+            <div class="card text-white bg-danger o-hidden h-100">
+              <div class="card-body">
+                <div class="mr-5">
+                  <i class="far fa-book-open"></i> Peminjaman: 23
+                </div>
+              </div>
+              <RouterLink class="nav-link" to="/peminjaman"
+                ><a href="" class="btn btn-danger">
+                  Detail <i class="fa fa-arrow-right"></i
+                ></a>
+              </RouterLink>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
